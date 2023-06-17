4061998c9ede1a92466c11575bff697ce603795a
David Mwangi <daudimwash@gmail.com>
2023-06-17 22:29:35 +0300

    Contains Duplicate - I

---
+++ arrays_and_hashing/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/contains-duplicate/
+ */
+
+//Time Complexity : O(n)
+//Space Complexity:  O(n)
+
+import java.lang.*;
+
+class Solution {
+    public boolean containsDuplicate(int[]nums){
+        Set<Integer>set = new HashSet<>();
+        int n =  nums.length;
+        for(int i =0; i < n; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
