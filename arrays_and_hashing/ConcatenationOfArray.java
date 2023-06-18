1a89f64bd46e14993554668144762906916968d6
David Mwangi <daudimwash@gmail.com>
2023-06-18 12:56:33 +0300

    Concatenation Of Array

---
+++ arrays_and_hashing/ConcatenationOfArray.java
@@ -0,0 +1,25 @@
+package arrays_and_hashing;
+
+/**
+ * https://leetcode.com/problems/concatenation-of-array/
+ * 
+ *  // i = 0
+    // j = n - 1
+ */
+
+public class ConcatenationOfArray {
+  public int[] getConcatenation(int[] nums){
+    int n =  nums.length;  
+    int i = 0;
+    int j = n;
+    int[] ans =  new int [n * 2];
+
+    while(i < n){
+        ans[i] = ans[j] =  nums[i];
+        i++;
+        j++;
+    }
+   
+    return ans;
+  }  
+}
