f04229fb636e8803552a4cd07917684273d53483
David Mwangi <daudimwash@gmail.com>
2023-06-20 07:22:11 +0300

    Unique Email Address without inbuilt functions

--- arrays_and_hashing/UniqueEmailAddresses.java
+++ arrays_and_hashing/UniqueEmailAddresses.java
@@ -8,6 +8,7 @@ package arrays_and_hashing;
  * m.y+name@email.com => my@email.com
  *    ^
  */
+//Solution 1
 public class UniqueEmailAddresses {
     public int numUniqueEmails(String [] emails){
         Set<String>result =  new HashSet<>();
@@ -19,6 +20,33 @@ public class UniqueEmailAddresses {
             String newLocal = local.split("\\+")[0].replace(".","");
             String finalEmail = newLocal.concat("@").concat(domain);
             result.add(finalEmail);
+
+        }
+        return result.size();
+    }
+}
+
+//Solution 2 
+public class UniqueEmailAddresses {
+    public int numUniqueEmails(String [] emails){
+        Set<String>result =  new HashSet<>();
+
+        for(String email: emails ){
+            int i = 0;
+            StringBuilder localSb = new StringBuilder();
+
+            while(email.charAt(i) != '@' && email.charAt(i) != '+'){
+                if(email.charAt(i) != '.'){
+                    localSb.append(email.charAt(i));
+                }
+                i++;
+            }
+            
+            while(email.charAt(i) != '@'){
+                i++;
+            }
+            
+            result.add(localSb.toString()+"@" + email.substring(i));
 
         }
         return result.size();
