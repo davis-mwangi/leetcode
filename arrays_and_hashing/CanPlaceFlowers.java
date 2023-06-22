e836d7f298a352c93c876949b4130f8a8ccbb16c
David Mwangi <daudimwash@gmail.com>
2023-06-22 23:59:43 +0300

    Can Place Flowers

---
+++ arrays_and_hashing/CanPlaceFlowers.java
@@ -0,0 +1,29 @@
+package arrays_and_hashing;
+/**
+ * https://leetcode.com/problems/can-place-flowers/
+ * 
+ * 
+ */
+public class CanPlaceFlowers {
+    
+    public boolean canPlaceFlowers(int[] flowerbed, int n ) {
+        
+        int len = flowerbed.length;
+
+        int [] f = new int[len + 2];
+        int x = 1;
+       
+        for (int i = 0; i < len; i++) {
+             f[x] = flowerbed[i];
+             x++;
+        }
+
+        for(int i = 1; i < f.length - 1 ; i++){
+            if(f[i - 1] == 0  && f[i] == 0 && f[i+1] == 0){
+                f[i] = 1;
+                n --;
+            }
+        }
+        return n <= 0; 
+    }
+}
