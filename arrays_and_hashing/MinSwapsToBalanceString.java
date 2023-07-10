065a9c7d331101d04795c66d716a07dc669914ce
David Mwangi <daudimwash@gmail.com>
2023-07-11 00:07:15 +0300

    1963. Minimum Number of Swaps to Make the String Balanced (Optimized)

--- arrays_and_hashing/MinSwapsToBalanceString.java
+++ arrays_and_hashing/MinSwapsToBalanceString.java
@@ -1,28 +1,51 @@
 package arrays_and_hashing;
+
 import java.util.*;
 
 /**
  * https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/description/
  * 
- * Formulae result = (count +  1 ) / 2
+ * Formulae result = (count + 1 ) / 2
+ * 
+ * 
  */
 public class MinSwapsToBalanceString {
-    
-    public int minSwaps(String s){
-        Stack<Character>stack = new Stack<>();
+
+    //Space Complexity: O(n)
+    //Time Complexity: O(n)
+    //Helpful Videos: https://www.youtube.com/watch?v=17hUpx4GdEA
+    public int minSwaps2(String s) {
+        Stack<Character> stack = new Stack<>();
         int count = 0;
-        for(char ch: s.toCharArray()){
-            if(ch == '['){
+        for (char ch : s.toCharArray()) {
+            if (ch == '[') {
                 stack.push(ch);
-            }else{
-                if(!stack.isEmpty() && ch ==']'){
+            } else {
+                if (!stack.isEmpty() && ch == ']') {
                     stack.pop();
-                }else{
+                } else {
                     count++;
                 }
             }
         }
         return (count + 1) / 2;
     }
-    
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    //Guide:  https://www.youtube.com/watch?v=3YDBT9ZrfaU
+    public int minSwaps(String s) {
+        int extraClosingbrackets = 0, max = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)== ']') {
+                extraClosingbrackets++;
+                max = Math.max(extraClosingbrackets, max);
+            }
+            else 
+                extraClosingbrackets--;
+        }
+
+        return (max + 1) / 2;
+    }
+
 }
