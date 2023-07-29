29182e924b22c297542fdfe3edd18fa969d8801e
David Mwangi <daudimwash@gmail.com>
2023-07-29 23:52:06 +0300

    162. Find Peak Element

---
+++ binary_search/FindPeakElement.java
@@ -0,0 +1,10 @@
+package binary_search;
+/**
+ * https://leetcode.com/problems/find-peak-element/
+ */
+public class FindPeakElement {
+
+    public int findPeakElement(int[] nums) {
+        
+    }
+}
