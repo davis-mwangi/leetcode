3c0ebcb58c79122f6d00855ce22987de8224ed29
David Mwangi <daudimwash@gmail.com>
2023-08-06 22:20:12 +0300

    1472. Design Browser History

---
+++ linked_list/DesignBrowserHistory.java
@@ -0,0 +1,50 @@
+package linked_list;
+/**
+ * 
+ */
+public class DesignBrowserHistory {
+    static class ListNode {
+        String val;
+        ListNode prev;
+        ListNode next;
+
+        public ListNode(String val){
+            this.val  = val;
+            this.prev =  null;
+            this.next =  null;
+        }
+
+        public ListNode(String val, ListNode prev, ListNode next){
+            this.val  = val;
+            this.prev =  prev;
+            this.next =  next;
+        }
+    }
+    static class BrowserHistory {
+        ListNode curr;
+        public BrowserHistory(String homepage) {
+            this.curr = new  ListNode(homepage);
+        }
+        
+        public void visit(String url) {
+            this.curr.next = new ListNode(url, this.curr, null);
+            this.curr = this.curr.next;
+        }
+        
+        public String back(int steps) {
+            while(curr.prev != null  && steps > 0){
+                curr =  curr.prev;
+                steps -= 1;
+            }
+            return curr.val;
+        }
+        
+        public String forward(int steps) {
+            while(curr.next != null && steps > 0){
+                curr = curr.next;
+                steps -= 1;
+            }
+            return curr.val;
+        }
+    }
+}
