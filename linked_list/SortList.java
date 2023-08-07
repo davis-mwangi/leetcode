ef9ad8e71ea0fe766aacbd07b12265bc12fd78cb
David Mwangi <daudimwash@gmail.com>
2023-08-08 00:11:52 +0300

    148. Sort List

--- linked_list/SortList.java
+++ linked_list/SortList.java
@@ -1,66 +1,80 @@
 package linked_list;
+
 /**
  * https://leetcode.com/problems/sort-list/
  */
 public class SortList {
 
-    public ListNode sortList(ListNode head) {
-            //Use merge sort recursively
+    // Use merge sort recursively
     public ListNode sortList(ListNode head) {
 
-        //Base case
-        if(head == null ||  head.next == null){
+        // Base case
+        if (head == null || head.next == null) {
             return head;
         }
 
-        //Split the list into two halfs
-        ListNode left =  head;
-        ListNode right =  getMiddleNode(head);
+        // Split the list into two halfs
+        ListNode left = head;
+        ListNode right = getMiddleNode(head);
         ListNode temp = right.next;
         right.next = null;
-        right =  temp;
+        right = temp;
 
-       return merge(left, right);
+        // Sort Recursion
+        left = sortList(left);
+        right = sortList(right);
+
+        return merge(left, right);
 
     }
-    private ListNode merge(ListNode l1, ListNode l2){
+
+    private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode();
         ListNode tail = dummy;
-        while(l1 != null && l2 != null){
-            if(l1.val < l2.val){
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
                 tail.next = l1;
                 l1 = l1.next;
-            }else{
+            } else {
                 tail.next = l2;
                 l2 = l2.next;
             }
             tail = tail.next;
         }
-        if(l1 != null){
+        if (l1 != null) {
             tail.next = l1;
         }
-        if(l2 != null){
+        if (l2 != null) {
             tail.next = l2;
         }
         return dummy.next;
     }
-    private ListNode getMiddleNode(ListNode head){
+
+    private ListNode getMiddleNode(ListNode head) {
         ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
         return slow;
     }
-    }
 
-    //Definition for singly-linked list.
-     public class ListNode {
-         int val;
-         ListNode next;
-         ListNode() {}
-         ListNode(int val) { this.val = val; }
-         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     }
+    // Definition for singly-linked list.
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
