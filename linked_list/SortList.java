d2633bd6db89675e62ce648c45950ebc5e7b5939
David Mwangi <daudimwash@gmail.com>
2023-08-08 00:00:03 +0300

    148. Sort List

---
+++ linked_list/SortList.java
@@ -0,0 +1,66 @@
+package linked_list;
+/**
+ * https://leetcode.com/problems/sort-list/
+ */
+public class SortList {
+
+    public ListNode sortList(ListNode head) {
+            //Use merge sort recursively
+    public ListNode sortList(ListNode head) {
+
+        //Base case
+        if(head == null ||  head.next == null){
+            return head;
+        }
+
+        //Split the list into two halfs
+        ListNode left =  head;
+        ListNode right =  getMiddleNode(head);
+        ListNode temp = right.next;
+        right.next = null;
+        right =  temp;
+
+       return merge(left, right);
+
+    }
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                tail.next = l1;
+                l1 = l1.next;
+            }else{
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if(l1 != null){
+            tail.next = l1;
+        }
+        if(l2 != null){
+            tail.next = l2;
+        }
+        return dummy.next;
+    }
+    private ListNode getMiddleNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    }
+
+    //Definition for singly-linked list.
+     public class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+}
