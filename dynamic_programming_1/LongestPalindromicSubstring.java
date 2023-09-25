6c15e50db1dad1da3f4bc6284703948c1881e81c
David Mwangi <daudimwash@gmail.com>
2023-09-25 18:01:32 +0300

    5. Longest Palindromic Substring

---
+++ dynamic_programming_1/LongestPalindromicSubstring.java
@@ -0,0 +1,40 @@
+package dynamic_programming_1;
+/*
+https://leetcode.com/problems/longest-palindromic-substring/description/
+
+
+// Time Complexity: O(n^2)
+// Extra Spce Complexity: O(n^2)
+
+YouTube : https://www.youtube.com/watch?v=6i_T5kkfv4A
+*/
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        int left = 0, right = 1, max = 0;
+
+        var isPalindrome = new boolean[len][len];
+
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i; j < len; j++) {
+                if (i == j) {
+                    isPalindrome[i][j] = true;
+                } else if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i == 1) {
+                        isPalindrome[i][j] = true;
+                    } else {
+                        isPalindrome[i][j] = isPalindrome[i + 1][j - 1];
+                    }
+                }
+
+                if (isPalindrome[i][j] && j - i + 1 > max) {
+                    max = j - i + 1;
+                    left = i;
+                    right = j + 1;
+                }
+            }
+        }
+
+        return s.substring(left, right);
+    }
+}
