cbf0e5545b555fd477d94fb04cfb251c437e554b
davis-mwangi <daudimwash@gmail.com>
2023-10-16 22:33:55 +0300

    Start DB on subsequences/subsets series

---
+++ dynamic_programming_2/dp_on_subsequences/SubsetSumEqualToK.java
@@ -0,0 +1,12 @@
+package dynamic_programming_2.dp_on_subsequences;
+/*
+1) Express in terms of index and target
+2) Explore possibilities of that index
+ arr[index] part of the subsequence or 
+   arr[index] not part of the index
+3) return true of false
+ */
+
+public class SubsetSumEqualToK {
+    
+}
