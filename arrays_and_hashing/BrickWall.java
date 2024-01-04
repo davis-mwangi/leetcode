fa0c3f073986b7cfa6bc949fbdda8c1bc6fa1271
David Mwangi <daudimwash@gmail.com>
2024-01-04 21:02:56 +0300

    554. Brick Wall

--- arrays_and_hashing/BrickWall.java
+++ arrays_and_hashing/BrickWall.java
@@ -1,30 +1,41 @@
 /**
  * https://leetcode.com/problems/brick-wall/description/
+ * 
+ * Algorithm:
+ *    Naive approach:
+ *        Take the sum of all bricks and then subsdivide then into portions of ones such that it will be  1 -> sum(N) -1
+ *         and then determine how many gaps we cut through, this will grow if the length of the total width increeases.
+ * 
+ *    Optimal Solution:
+ *        We can consider the gaps  at each row, with help of a hashmap we can maintain the gap position as the key and value as 
+ *       the count of gaps at that position.
+ *       - at the end we position with most gaps will be the one with minimal cuts.
+ *       - Then we can get min cuts as total number of rows - max gaps
+ *     
+ *      TC: O(M * N)
+ *      SC: O(M * N )
  */
-//Time Complexity: O(m * n)
-//Space Complexity:  O(m * n)
-//Trick, take gap's position as keys and count number of gaps
-//Take the size - maximum gaps
-public class Solution {
 
+import java.util.*;
+class Solution {
     public int leastBricks(List<List<Integer>> wall) {
-
-        //Create a gap count map
-        HashMap<Integer,Integer>gapCountMap = new HashMap<>();
-
-        int n = wall.size();
-        int maxGaps = 0;
-        
-        for(List<Integer> row: wall){
-           int pos = 0;
-           for(int i = 0; i < row.size() -1; i++){
-              pos += row.get(i);
-              gapCountMap.put(pos, gapCountMap.getOrDefault(pos, 0) +1 );
-              maxGaps = Math.max(maxGaps, gapCountMap.get(pos));
-           }
-        }
-        return n - maxGaps;
-        
-    }
-
-}
+         Map<Integer,Integer>map = new HashMap<>(); // {gap position :  count of gaps in that position}
+         int maxPositionGaps = 0;
+         int rows = wall.size();
+         for(List<Integer>row :  wall){
+             
+             int position = 0;
+  
+             //Ignore gaps at 0 and n -1 position since are edges
+             for(int i = 0; i < row.size() -1; i ++){
+                 int brickWidth = row.get(i);
+                 position +=  brickWidth;
+                 map.put( position, map.getOrDefault(position, 0) + 1 );
+                 
+                 maxPositionGaps = Math.max(maxPositionGaps, map.get(position));
+             }
+         }
+         return rows - maxPositionGaps;
+     }
+  
+  }
