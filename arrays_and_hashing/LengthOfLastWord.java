d7291e29dd18e73e896eeb929f8dfdd496d33e00
David Mwangi <daudimwash@gmail.com>
2024-01-05 22:49:41 +0300

    58. Length of Last Word

--- arrays_and_hashing/LengthOfLastWord.java
+++ arrays_and_hashing/LengthOfLastWord.java
@@ -3,6 +3,12 @@ package arrays_and_hashing;
 /**
  * https://leetcode.com/problems/length-of-last-word/
  * 
+ * Algorithm:
+ * 
+ *    Since we want to count the length  of last word,
+ *       We can approach the question from the end, and skip the white spaces,
+ *        then count until we get white space, and return the length;
+ * 
  * Time Complexity  O(n)
  * Space Complexity O(1)
  */
