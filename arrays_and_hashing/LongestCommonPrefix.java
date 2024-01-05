fbfc4e16eb5e4d6fd077d884372683a20f720aab
David Mwangi <daudimwash@gmail.com>
2024-01-05 22:57:47 +0300

    14. Longest Common Prefix

--- arrays_and_hashing/LongestCommonPrefix.java
+++ arrays_and_hashing/LongestCommonPrefix.java
@@ -1,6 +1,13 @@
 package arrays_and_hashing;
 /**
  * https://leetcode.com/problems/longest-common-prefix/
+ * 
+ * Algorithm
+ *     Pick one string, iterate from first  character to end , 
+ *        loop through each word and compare it with character position in string picked first
+ *     
+ *       Note edge case where string picked is smaller,  whenenever i becomes bigger than current word we just return
+ *       the formed characters. 
  *
  * ["flower", "flow","flight"]
  *  str = flower
