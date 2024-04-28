e26f8f546a6e4f497b1460879052ee45fd18d61e
David Mwangi <daudimwash@gmail.com>
2024-04-29 00:46:02 +0300

    141. Linked List Cycle

---
+++ Neetcode_450_Questions/LinkedListCycle_Easy_LC141.java
@@ -0,0 +1,33 @@
+package Neetcode_450_Questions;
+
+/*
+https://leetcode.com/problems/linked-list-cycle/description/
+*/
+public class LinkedListCycle_Easy_LC141 {
+    // Definition for singly-linked list.
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
