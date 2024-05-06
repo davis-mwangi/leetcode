e7107e063b19611b290146fa262830c018a9a54c
David Mwangi <daudimwash@gmail.com>
2024-05-06 09:25:41 +0300

    100. Same Tree

---
+++ Neetcode_450_Questions/SameTree_Easy_LC.java
@@ -0,0 +1,54 @@
+package Neetcode_450_Questions;
+
+/*
+https://leetcode.com/problems/same-tree/description/
+*/
+public class SameTree_Easy_LC {
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+
+            return dfs(p, q);
+        }
+
+        public boolean dfs(TreeNode p, TreeNode q) {
+
+            if (p == null && q == null) {
+                return true;
+            }
+
+            if (p == null || q == null) {
+                return false;
+            }
+
+            if (p.val != q.val) {
+                return false;
+            }
+
+            boolean left = dfs(p.left, q.left);
+            boolean right = dfs(p.right, q.right);
+
+            return left && right;
+        }
+    }
+}
