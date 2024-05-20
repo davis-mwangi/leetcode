af6f7fc966d432c1be0333d882f35d8bf47f8923
David Mwangi <daudimwash@gmail.com>
2024-05-21 00:43:57 +0300

    179. Largest Number

---
+++ Neetcode_450_Questions/MEDIUM/LargestNumber_Medium_LC179.java
@@ -0,0 +1,53 @@
+package Neetcode_450_Questions.MEDIUM;
+/*
+https://leetcode.com/problems/largest-number/description/
+
+*/
+import java.util.*;
+
+public class LargestNumber_Medium_LC179 {
+    class Solution {
+        /*
+        [3,30,34,5,9] => "9534330"
+    
+        */
+        public String largestNumber(int[] nums) {
+            int n =  nums.length;
+            //convert int nums to string array
+            String [] strs = new String[n];
+            
+            for(int i = 0; i < n; i++){
+                strs[i] = Integer.toString(nums[i]);
+            }
+            //Sort the string per  size
+            Arrays.sort(strs, new CustomComparator() );
+    
+            //Handle case when res is "00000" we need to return "0"
+            String large = String.join("", strs);
+            
+            // Remove trailing zeros
+            StringBuilder sb = new StringBuilder(large);
+            
+            if(sb.charAt(0) == '0'){
+                int i = 0;
+                while(i < sb.length() && sb.charAt(i) == '0' ){   
+                    sb.deleteCharAt(i);
+                    i++; 
+                }
+            }
+            return sb.toString();
+        }
+    
+    
+        static class CustomComparator implements Comparator<String> {
+            @Override
+            public int compare(String str1, String str2){
+                if( Long.parseLong(str1+str2) > Long.parseLong(str2 + str1) ){
+                    return -1;
+                }else {
+                    return 1;
+                }
+            }
+        }
+    }
+}
