eae7ddd725c8e8e616a8d7aa9cb4901ae076c98c
David Mwangi <daudimwash@gmail.com>
2024-06-18 18:00:49 +0300

    Detect Cycle in a Graph

---
+++ graphs/DetectCycleInGraphBFS.java
@@ -0,0 +1,55 @@
+package graphs;
+
+import java.util.*;
+
+/*
+https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=detect-cycle-in-an-undirected-graph
+
+*/
+
+public class DetectCycleInGraphBFS {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] visited = new boolean[V];//By default is false
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (detectCycle(i, V, adj, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private boolean detectCycle(int source, int V, ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
+        //Mark as visited
+        visited[source] = true;
+        Deque<Integer[]> deque = new LinkedList<>();
+        deque.add(new Integer[]{source, -1}); //[node, source]
+
+        while (!deque.isEmpty()) {
+            //  int size = deque.size();
+
+            //  for(int i = 0; i < size; i++){
+
+            Integer[] pair = deque.poll();
+            int node = pair[0];
+            int parent = pair[1];
+
+            for (Integer neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    deque.add(new Integer[]{neighbor, node});
+                } else if (neighbor != parent) {//If the neighbor is not the source, then there must be a cycle on the graph
+                    return true;
+
+                }
+            }
+
+            //  }
+        }
+        return false; //No Cycle
+    }
+}
---
+++ graphs/DetectCycleInGraphDFS.java
@@ -0,0 +1,41 @@
+package graphs;
+
+import java.util.*;
+
+/*
+https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=detect-cycle-in-an-undirected-graph
+
+*/
+class DetectCycleInGraphDFS {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+         boolean[] visited = new boolean[V];//By default is false
+         for(int i = 0; i < V; i++){
+             if(!visited[i]){
+                if(detectCycleDFS(i, V, adj, visited)){
+                    return  true;
+                }
+             }
+         }
+         return false;
+    }
+    
+    private boolean detectCycleDFS(int node, int parent, ArrayList<ArrayList<Integer>> adj, boolean[] visited){
+        //Mark as visited
+        visited[node] = true;
+        
+        for(int neigh : adj.get(node)){
+            if(!visited[neigh]){
+                 //If we get there is a cycle, return it immediately
+                 if(detectCycleDFS(neigh, node, adj, visited)){
+                     return true;
+                 }
+            }else if(neigh != parent){ //Visited and is not parent, them some body must have gone through the node, so there a cycle
+                 return true;
+            }
+        }
+        return false;//No Cycle
+        
+    }
+}
