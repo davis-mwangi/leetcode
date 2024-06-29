d702daab726c5fd059af7ffbd7b6b8c19251ec4e
David Mwangi <daudimwash@gmail.com>
2024-06-29 22:50:51 +0300

    339. Nested List Weight Sum

--- Neetcode_450_Questions/MEDIUM/NestedListWeightSum_LC551.java
+++ Neetcode_450_Questions/MEDIUM/NestedListWeightSum_LC551.java
@@ -1,6 +1,38 @@
 package Neetcode_450_Questions.MEDIUM;
 
 /*
+You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists.
+
+The depth of an integer is the number of lists that it is inside of. For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth.
+
+Return the sum of each integer in nestedList multiplied by its depth.
+
+ 
+
+Example 1:
+
+
+Input: nestedList = [[1,1],2,[1,1]]
+Output: 10
+Explanation: Four 1's at depth 2, one 2 at depth 1. 1*2 + 1*2 + 2*1 + 1*2 + 1*2 = 10.
+Example 2:
+
+
+Input: nestedList = [1,[4,[6]]]
+Output: 27
+Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3. 1*1 + 4*2 + 6*3 = 27.
+Example 3:
+
+Input: nestedList = [0]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nestedList.length <= 50
+The values of the integers in the nested list is in the range [-100, 100].
+The maximum depth of any integer is less than or equal to 50.
+
 
 Question : https://mrleonhuang.gitbooks.io/lintcode/content/data-structure/nested-list-weight-sum.html
 Guide : https://www.youtube.com/watch?v=jR2UC4K-q2U
@@ -57,4 +89,20 @@ public class NestedListWeightSum_LC551 {
         }
         return sum;
     }
+
+        // DFS SOLUTION 
+        public int depthSum2(List<NestedInteger> nestedList) {
+            return depthSum(nestedList, 1);
+        }
+        public int depthSum(List<NestedInteger> nestedList, int level) {
+            int result = 0;
+            for(NestedInteger ni : nestedList) {
+                if (ni.isInteger()) {
+                    result = result + (level * ni.getInteger());
+                }else {
+                    result = result + depthSum(ni.getList(), level+1);
+                }
+            }
+            return result;
+        }
 }
