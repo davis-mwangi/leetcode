35bab1e9ef5e9352ff2757ed60d5ca7496503ee4
David Mwangi <daudimwash@gmail.com>
2024-06-30 01:55:20 +0300

    680. Valid Palindrome II

--- Neetcode_450_Questions/ValidPalindromeII_Easy_LC680.java
+++ Neetcode_450_Questions/ValidPalindromeII_Easy_LC680.java
@@ -41,4 +41,39 @@ public class ValidPalindromeII_Easy_LC680 {
             return true;
         }
     }
+
+
+    class Solution2 {
+        /*
+         "abca"
+         rightSkip =  abc 
+         leftSkip =   bca
+        check if palindrome
+    
+        */
+        public boolean validPalindrome(String s) {
+              int l = 0;
+              int r =  s.length() - 1;
+              while(l <= r){
+               
+                if(s.charAt(l) !=  s.charAt(r)){
+                    //Check if palindrome by skipping either right or left character
+                    return ( isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1) );
+                }
+                l++;
+                r--;
+            }
+            return true;
+        }
+        private  boolean isPalindrome(String s, int left, int right){
+            while(left <= right){
+               if(s.charAt(left) != s.charAt(right)){
+                return false;
+               }
+               left++;
+               right--;
+            }
+            return true;
+        }
+    }
 }
