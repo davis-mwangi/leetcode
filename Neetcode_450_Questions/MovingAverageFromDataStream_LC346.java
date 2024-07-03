2f0a0b8c02437ee7dd996074813d26c18e0f678b
David Mwangi <daudimwash@gmail.com>
2024-07-03 13:57:10 +0300

    MovingAverageFromDataStream_LC346

--- Neetcode_450_Questions/MovingAverageFromDataStream_LC346.java
+++ Neetcode_450_Questions/MovingAverageFromDataStream_LC346.java
@@ -1,23 +1,49 @@
 package Neetcode_450_Questions;
+
 /*
 
 https://leetcode.ca/all/346.html
 
 */
-public class MovingAverageFromDataStream_LC346 {
-    class MovingAverage{
-         int maxDivisor = 0;
-         int count = 0;
-         int sum = 0;
-         MovingAverage(int val){
-             this.maxDivisor = val;
-         }
-        int next(int val){
-            this.count += 1;
-            int counter = Math.min(this.count, maxDivisor);
-            this.sum += val;
+import java.util.*;
 
-           return sum  / counter;
+public class MovingAverageFromDataStream_LC346 {
+
+    // Time Complexity :  O(1)
+    // Space Complexity: O(N)
+    class MovingAverage {
+        Deque<Integer> deque;
+        int count = 0;
+        int size = 0;
+        int currSum = 0;
+
+        MovingAverage(int size) {
+            this.deque = new LinkedList<>();
+            this.size = size;
+        }
+
+        double next(int val) {
+
+            if (deque.size() < size) {
+
+                this.deque.add(val);
+                currSum += val;
+
+            } else {
+                // Remove value to the left
+                int leftVal = this.deque.pollFirst();
+                currSum -= leftVal;
+
+                // Add value to the right
+                deque.add(val);
+                currSum += val;
+            }
+            if (count < size) {
+                count += 1;
+            }
+
+            return (double) currSum / (double) count;
+
         }
     }
 }
